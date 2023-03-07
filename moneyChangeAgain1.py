0dc3c72f9d31a536bd7b7459f76a5a25145d5f28
Alex Slone <alexjslone12@gmail.com>
2023-03-07 10:59:43 +0700

    solution to money change probably. Filled an array from bottom up and based off of the previous value found the next value.

--- moneyChangeAgain1.py
+++ moneyChangeAgain1.py
@@ -1,10 +1,28 @@
+moneyList = []
+coins = [1, 3, 4]
 def change(money):
-    # write your code here
+    for i in range(0,money+1): 
+        moneyList.append(float('inf'))
+    for m in range(0,money+1):
+        #moneyList[m]=float('inf')
+        for c in coins: 
+            #moneyList[m]=float('inf')
+            #I'm not sure if I need to move this up?
+            if m == 0: 
+                moneyList[m]=0
+            elif m >= c and moneyList[m-c]+1 <= moneyList[m]:
+                moneyList[m]= 1 + moneyList[m-c]
+                #print(moneyList[m])
+                #this should give you the min value for each
+    return moneyList[money]
 
-    return money
 
-"""
+
+    
+#money = 34
+#print(change(money))
+
+
 if __name__ == '__main__':
     m = int(input())
     print(change(m))
-"""
